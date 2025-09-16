efeae97dce4ef2467a828a9e081ce0dc5ddbba81
DavidFlautero <davsarg33@gmail.com>
2025-09-16 17:51:44 -0300

    n17: Remove LogoCloud

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,7 @@
-import Testimonials from '@/components/landing-page/testimonials-default';
+ import Testimonials from '@/components/landing-page/testimonials-default';
 import FAQSection from '@/components/landing-page/faq';
 import Hero from '@/components/landing-page/hero';
-
+import LogoCloud from '@/components/landing-page/logo-cloud-svg';
 import FeaturesHover from '@/components/landing-page/features-hover';
 import Pricing from '@/components/pricing/pricing-primary';
 
@@ -9,7 +9,7 @@ export default async function IndexPage() {
   return (
     <div className="flex-col gap-10 mb-5">
       <Hero />
-      
+      <LogoCloud />
       <FeaturesHover />
       <Pricing />
       <Testimonials />
